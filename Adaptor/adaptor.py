2f1fb33939cc50dd4766bd3e0b6b42e37bc65746
jimin <jiminaleejung@gmail.com>
2021-11-04 00:39:31 +0900

    adaptor 코드 작성 중

--- Adaptor/adaptor.py
+++ Adaptor/adaptor.py
@@ -1,31 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_mongoengine import MongoEngine
+from flask_sqlalchemy import Model
+from pymongo import MongoClient
 
 app = Flask(__name__)
-db = MongoEngine(app)
-sql_db = SQLAlchemy()
 
-app.config['MONGODB_SETTINGS'] = {
-    'db': 'study',
-    'host': '127.0.0.1',
-    'port': 27017,
-}
+sql = SQLAlchemy()
+client = MongoClient('mongodb://localhost:27017')
+db = client['study']
 
 
-class User(sql_db.Model):
-    name = sql_db.Column(sql_db.String(15), nullable=False)
-    age = sql_db.Column(sql_db.Integer, nullable=True)
+class User(sql.Model):
+    name = sql.Column(sql.String(15), nullable=False)
+    age = sql.Column(sql.Integer, nullable=True)
 
 
-class User(db.Document):
-    name = db.StringField(max_length=15, required=True)
-    age = db.IntegerField()
 
-
-class MongoAlchemy(sql_db):
-    def __init__(self, model_class=A
-    
-    
-    , **kwargs) -> None:
-        super().__init__(**kwargs)
+class MongoAlchemy(Model):
+    pass
